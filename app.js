2004649f9216834c3b12d896372482023d91bdfd
Didier Nsengiyumva <nsedidier@gmail.com>
2025-01-28 10:22:08 +0200

    Update app.js 1022

--- app.js
+++ app.js
@@ -570,6 +570,7 @@ const handleDocumentUpload = async (message, phone, phoneNumberId) => {
     };
 
     // 6. Save to Firestore
+    let docRef;
     try {
       await firestore.collection("whatsappInsuranceOrders").add(insuranceData);
       console.log("Document reference saved to Firestore");
